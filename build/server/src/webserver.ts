54d44f2ce4848049296a7d6c4c63f5e1dbf03dd5
dapplion <guillem.lion.benosa@gmail.com>
2019-10-06 16:14:13 +0200

    Implement cache and support react-router

--- build/server/src/webserver.ts
+++ build/server/src/webserver.ts
@@ -1,13 +1,21 @@
 import express from "express";
+import compression from "compression";
+import path from "path";
 const app = express();
 
 const port = process.env.WEBSERVER_PORT || 8080;
-const filesPath = process.env.WEBSERVER_PATH || "dist";
+const filesPath = path.resolve(process.env.WEBSERVER_PATH || "dist");
+const indexFile = path.resolve(filesPath, "index.html");
 
-app.use(express.static(filesPath));
+// Express uses "ETags" (hashes of the files requested) to know when the file changed
+app.use(express.static(filesPath, { maxAge: "1d" }));
+app.use(compression());
+
+// React-router needs the index.html at other routes
+app.get("*", (_0, res) => res.sendFile(indexFile));
 
 export default function runWebServer() {
-  app.listen(port, () =>
-    console.log(`Webserver on ${port} serving ${filesPath}`)
-  );
+  app.listen(port, () => console.log(`Webserver on ${port}, ${filesPath}`));
 }
+
+runWebServer();
