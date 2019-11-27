29e70a1bcdbd943062a19088b3f9dc73b40dbbaa
dapplion <guillem.lion.benosa@gmail.com>
2019-11-27 10:52:09 +0100

    Crossplatform colorable dot

--- build/ui/src/Assets/PinStatusDot.tsx
+++ build/ui/src/Assets/PinStatusDot.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { useTheme } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import { PinStatus } from "../types";
 import statusColorMap from "../components/statusColorMap";
+import LensIcon from "@material-ui/icons/Lens";
+
+const useStyles = makeStyles(theme => ({
+  statusCell: {
+    display: "flex",
+    alignItems: "center"
+  }
+}));
 
 export default function PinStatusDot({ status }: { status: PinStatus }) {
   // Load theme for colors
   const theme: any = useTheme();
+  const classes = useStyles();
 
   return (
-    <span>
-      <span
-        style={{ color: statusColorMap(status, theme) }}
-        role="img"
-        aria-label="status"
-      >
-        🌑
-      </span>
+    <span className={classes.statusCell}>
+      <LensIcon
+        style={{ color: statusColorMap(status, theme), fontSize: "1rem" }}
+      />
       <span style={{ marginLeft: "0.5rem" }}>{status}</span>
     </span>
   );
