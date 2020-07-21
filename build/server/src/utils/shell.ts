1c325d84db50287eb1c02debfbf6810668bb9897
dapplion <guillem.lion.benosa@gmail.com>
2020-07-21 14:01:12 +0200

    Remove unnecessary shell error code

--- build/server/src/utils/shell.ts
+++ build/server/src/utils/shell.ts
@@ -1,5 +1,5 @@
 import dargs from "dargs";
-import { exec, ExecException, ExecOptions } from "child_process";
+import { exec, ExecOptions } from "child_process";
 
 /**
  * If timeout is greater than 0, the parent will send the signal
@@ -26,22 +26,17 @@ export async function shell(
   if (!options.timeout) options.timeout = defaultTimeout;
   if (!options.maxBuffer) options.maxBuffer = defaultMaxBuffer;
 
-  return new Promise(
-    (resolve, reject): void => {
-      exec(cmd, options, (err, stdout, stderr) => {
-        if (err) {
-          // Rethrow a typed error, and ignore the internal NodeJS stack trace
-          if (err.signal === "SIGTERM")
-            reject(
-              new ShellError(err, `cmd timeout ${options.timeout}: ${cmd}`)
-            );
-          else reject(new ShellError(err));
-        } else {
-          resolve(stdout.trim() || stderr);
-        }
-      });
-    }
-  );
+  return new Promise((resolve, reject): void => {
+    exec(cmd, options, (err, stdout, stderr) => {
+      if (err) {
+        if (err.signal === "SIGTERM")
+          err.message = `cmd timeout ${options.timeout}: '${cmd}'. ${err.message}`;
+        reject(err);
+      } else {
+        resolve(stdout.trim() || stderr);
+      }
+    });
+  });
 }
 
 /**
@@ -55,29 +50,4 @@ export async function shellArgs(
 ): Promise<string> {
   const args = dargs(kwargs, { useEquals: false }).join(" ");
   return await shell(`${command} ${args}`, options);
-}
-
-/**
- * Typed error implementing the native node child exception error
- * Can be rethrow to ignore the internal NodeJS stack trace
- */
-export class ShellError extends Error implements ExecException {
-  cmd?: string;
-  killed?: boolean;
-  code?: number;
-  signal?: string;
-  stdout?: string;
-  stderr?: string;
-  constructor(
-    e: ExecException & { stdout?: string; stderr?: string },
-    message?: string
-  ) {
-    super(message || e.message);
-    this.cmd = e.cmd;
-    this.killed = e.killed;
-    this.code = e.code;
-    this.signal = e.signal;
-    this.stdout = e.stdout;
-    this.stderr = e.stderr;
-  }
 }
